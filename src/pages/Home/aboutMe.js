2885b2aeb7ba78abf8ae0c312d6ca673a3eefe56
damiankuchta <damkuch@gmail.com>
2022-06-02 03:04:29 +0100

    descriptions/readme

--- src/pages/Home/aboutMe.js
+++ src/pages/Home/aboutMe.js
@@ -1,3 +1,6 @@
-let aboutMe = `Hello World! My name is Damian I self-taught web developer, with no professional experience`
+let aboutMe = `Hello World! My name is Damian I self-taught web developer, with no professional experience, I was always
+interested in IT and any topics related to it, My first contact with programming was at age 15, when I was introduced to
+basics of C++, and even though I never got past basics every now and then I was coming back to coding, mostly for fun, 
+now I have deiced to take it onto a higher level and try myself to become professional.`
 
 export default aboutMe
